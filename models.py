c1b3ebf4078dfa1d68db8f8a40be30862d04dfdc
Erioluwa-dev <erioluwafawehinmi@gmail.com>
2026-08-01 20:22:31 +0100

    changes

--- models.py
+++ models.py
@@ -1,3 +1,15 @@
+from typing import Optional
+from datetime import datetime
+from sqlmodel import SQLModel, Field
+
+class Node(SQLModel, table=True):
+    id: Optional[int] = Field(default=None, primary_key=True)
+    title: str
+    content: str
+    tags: str = ""
+    created_at: datetime = Field(default_factory=datetime.utcnow)
+
+
 class Edge(SqlModel, table=True):
     id: Optional[int]= Field(default=None , primary_key=True)
     source_id: int = Field(foreign_key="node.id")
