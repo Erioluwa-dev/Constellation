b63325137bdb6d9143618d4bf3a18d33f8cf85e9
Erioluwa-dev <erioluwafawehinmi@gmail.com>
2026-08-02 12:59:28 +0100

    added node endpoint to main.py

--- main.py
+++ main.py
@@ -1,7 +1,15 @@
 from fastapi import FastAPI
+from models import Node
+from sqlmodel import Session
+from database import engine
+
 
 app = FastAPI(title="Constellation API")
 
 @app.get("/health")
 def health_check():
     return {"status": "ok"}
+
+@app.post("/nodes")
+def create_node(node:Node):
+    return {"node": "created"}
