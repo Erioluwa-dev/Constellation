85692b867f37071b6f85597eb94703154b5033cc
Erioluwa-dev <erioluwafawehinmi@gmail.com>
2026-08-02 18:42:13 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,15 +1,25 @@
 from fastapi import FastAPI
-from models import Node
 from sqlmodel import Session
 from database import engine
+from models import Node
 
 
 app = FastAPI(title="Constellation API")
+
+@app.get("/")
+def main():
+    return {"message": "welcome to Constellation"}
 
 @app.get("/health")
 def health_check():
     return {"status": "ok"}
 
 @app.post("/nodes")
-def create_node(node:Node):
-    return {"node": "created"}
+def create_node(node: Node):
+    with Session(engine) as session:
+        session.add(node)
+        session.commit()
+        session.refresh(node)
+        return node
+
+
