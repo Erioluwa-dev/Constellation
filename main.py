f9170be99c8e1550bc7c64fea34adb7b4a9405f2
Erioluwa-dev <erioluwafawehinmi@gmail.com>
2026-08-03 09:50:42 +0100

    added get and delete

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
+from platform import node
+from fastapi import HTTPException
+
 from fastapi import FastAPI
-from sqlmodel import Session
+from sqlalchemy.sql.roles import LimitOffsetRole
+from sqlalchemy.sql.util import _offset_or_limit_clause
+from sqlmodel import Session, select
 from database import engine
 from models import Node
 
@@ -23,3 +28,19 @@ def create_node(node: Node):
         return node
 
 
+@app.get("/nodes")
+def list_nodes():
+    with Session(engine) as session:
+        nodes = session.exec(select(Node)).all()
+        return nodes
+
+
+@app.delete("/nodes/{node_id}")
+def delete_nodes(node_id: int):
+    with Session(engine) as session:
+        node= session.get(Node, node_id)
+        if not node:
+            raise HTTPException(status_code=404, detail="Node not found")
+            session.delete(node)
+            session.commit()
+            return {"Successful":"Deleted node"}
